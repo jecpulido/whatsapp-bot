a38e541d2972d676765652932bb18050f16ef692
Jorge Pulido <jorgepulido95@hotmail.com>
2022-09-12 11:26:57 -0500

    make it better

--- index.js
+++ index.js
@@ -1,63 +1,68 @@
-require('dotenv').config()
-const fs = require("fs");
-const qrcode = require("qrcode-terminal");
-const { Client } = require("whatsapp-web.js");
+require('dotenv').config();
+const cron = require('node-cron');
+const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
-// Path where the session data will be stored
-const SESSION_FILE_PATH = "./session.json";
-// Environment variables
 const country_code = process.env.COUNTRY_CODE;
 const number = process.env.NUMBER;
 const msg = process.env.MSG;
 
-// Load the session data if it has been previously saved
-let sessionData;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
-}
+const approveMsg = [
+    'Me alegra amor, ¡Te amo!',
+    'Que bueno, ¡recuerda que eres hermosa!',
+    'Casi que no jeje me gustas',
+    'Eso es bueno, ¡Cuidate mucho!',
+    'Que bien que ya la tomaste Muak',
+]
+
+const disapproveMsg = [
+    '¡Tomatela ya! lok',
+    'Y que esperas >:',
+    'Por favor, tomatela',
+    '¡No me hagas ir hasta alla!',
+    'Mas vale que te la tomes',
+]
+
 
 const client = new Client({
-    session: sessionData,
-});
-
-client.initialize();
-
-client.on("qr", (qr) => {
+    authStrategy: new LocalAuth({
+      clientId: "client-one"
+    })
+  });
+  
+  client.initialize();
+  
+  client.on("qr", (qr) => {
     qrcode.generate(qr, { small: true });
-});
+  });
+  
+  client.on('authenticated', (session) => {
+    console.log('WHATSAPP WEB => Authenticated -> Session ->' & session);
+  });
+  
+  client.on("ready", async () => {
+    console.log("WHATSAPP WEB => Ready");
+  });
 
-// Save session values to the file upon successful auth
-client.on("authenticated", (session) => {
-    sessionData = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
-});
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
-client.on("auth_failure", msg => {
-    // Fired if session restore was unsuccessfull
-    console.error('AUTHENTICATION FAILURE', msg);
-})
-
-
-client.on("ready", () => {
-    console.log("Client is ready!");
-
-    setTimeout(() => {
-      let chatId = `${country_code}${number}@c.us`;
-        client.sendMessage(chatId, msg).then((response) => {
-            if (response.id.fromMe) {
-                console.log("It works!");
-            }
-        })
-    }, 5000);
-});
-
-client.on("message", message => {
-    if (message.body === "Hello") {
-        client.sendMessage(message.from, 'World!');
+client.on('message', message => {
+    if (message.body.includes('1') || message.body.toLowerCase === 'si') {
+        message.reply(approveMsg[getRandomInt(5)]);
+    }
+    if (message.body.includes('2') || message.body.toLowerCase === 'no') {
+        message.reply(disapproveMsg[getRandomInt(5)]);
     }
 });
 
+cron.schedule('0 11 * * *', () => {
+    let chatId = `${country_code}${number}@c.us`;
+    client.sendMessage(chatId, msg).then((response) => {
+        if (response.id.fromMe) {
+            console.log("It works!");
+        }
+    });
+});
