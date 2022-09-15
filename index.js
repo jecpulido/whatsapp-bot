90783e8e529a4078330fcf0053baeffcb235cf81
Jorge Pulido <jorgepulido95@hotmail.com>
2022-09-14 19:57:05 -0500

    Primera version

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const country_code = process.env.COUNTRY_CODE;
 const number = process.env.NUMBER;
+const number_admin = process.env.NUMBER_admin;
 const msg = process.env.MSG;
 
 const approveMsg = [
@@ -41,15 +42,25 @@ const client = new Client({
     console.log('WHATSAPP WEB => Authenticated -> Session ->' & session);
   });
   
-  client.on("ready", async () => {
-    console.log("WHATSAPP WEB => Ready");
-  });
+client.on("ready", async () => {
+  let msgAdmin = "WHATSAPP WEB => Ready"; 
+    console.log(msgAdmin);
+    setTimeout(() => {
+        let chatId = `${country_code}${number_admin}@c.us`;
+        client.sendMessage(chatId, msgAdmin).then((response) => {
+            if (response.id.fromMe) {
+                console.log("It works!");
+            }
+        });
+    }, 5000);
+});
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
 client.on('message', message => {
+    console.log("Llegó un mensaje!");
     if (message.body.includes('1') || message.body.toLowerCase === 'si') {
         message.reply(approveMsg[getRandomInt(5)]);
     }
@@ -58,11 +69,20 @@ client.on('message', message => {
     }
 });
 
-cron.schedule('0 11 * * *', () => {
+cron.schedule('15 11 * * *', () => {
     let chatId = `${country_code}${number}@c.us`;
     client.sendMessage(chatId, msg).then((response) => {
         if (response.id.fromMe) {
             console.log("It works!");
         }
     });
+});
+
+cron.schedule('30 23 * * *', () => {
+  let chatId = `${country_code}${number}@c.us`;
+  client.sendMessage(chatId, "¡Recuerda que me gustas mucho!").then((response) => {
+      if (response.id.fromMe) {
+          console.log("It works!");
+      }
+  });
 });
